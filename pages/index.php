2521bcb01bec45d9151e0684ea9ab87b5bb4c2ad
Anno <56979064+moonixx@users.noreply.github.com>
2026-09-18 22:24:16 +0200

    0.0.1
    Website is online

---
+++ pages/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Anno Austin | Under Construction</title>
+<link href="../css/index.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	
+	
+	<nav class="navbar">
+		
+		<img src="../assets/img/Logo Mark Black@4x.png" alt="" class="logo"/>
+		<ul>	
+			<li><a href="">Graduate Collection</a></li>
+			<li><a href="">Store</a></li>
+			<li><a href="">Work with me</a></li>
+		</ul>
+		<div></div>
+		<div></div>
+	  	<ul>
+			<li><a href="">Instagram</a></li>
+			<li><a href="">Youtube</a></li>
+		</ul>	
+	</nav>
+	
+	
+	
+</body>
+</html>
